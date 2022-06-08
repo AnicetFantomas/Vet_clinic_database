2b3d7982c6d7676443fa4ccf8190edbea3b2f0a7
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-06-08 12:06:48 +0200

    Add queries

--- queries.sql
+++ queries.sql
@@ -2,3 +2,9 @@
 
 SELECT * FROM animals WHERE name LIKE '%mon%';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE escapes_attempts <3 AND neutered IS TRUE;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT (name, escapes_attempts) FROM animals WHERE weight_kg >10.5;
+SELECT * FROM animals WHERE neutered IS true;
+SELECT * FROM animals WHERE name IN ('Agumon');
+SELECT * FROM animals WHERE weight_kg >=10.4 AND weight_kg <=17.3;
