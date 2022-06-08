33483b210067d246d2b7a41a3b3cbc457e03bc65
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-06-08 11:16:45 +0200

    Add data to database

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE public.animals
+(
+    id integer NOT NULL,
+    name character(100)[] NOT NULL,
+    date_of_birth date NOT NULL,
+    escapes_attempts integer NOT NULL,
+    neutered boolean NOT NULL,
+    weight_kg real NOT NULL,
+    PRIMARY KEY (id)
 );
+
+ALTER TABLE IF EXISTS public.animals
+    OWNER to postgres;
