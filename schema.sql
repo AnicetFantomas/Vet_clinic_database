ef87b391b32b3f5486d91a97f97d86e51b7d2c79
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-06-09 11:17:22 +0200

    Update Schemas.sql

--- schema.sql
+++ schema.sql
@@ -1,15 +1,18 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE public.animals
+CREATE TABLE IF NOT EXISTS public.animals
 (
     id integer NOT NULL,
-    name character(100)[] NOT NULL,
     date_of_birth date NOT NULL,
     escapes_attempts integer NOT NULL,
     neutered boolean NOT NULL,
     weight_kg real NOT NULL,
-    PRIMARY KEY (id)
-);
+    name character varying(100) COLLATE pg_catalog."default",
+    species character(200) COLLATE pg_catalog."default",
+    CONSTRAINT animals_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.animals
     OWNER to postgres;
