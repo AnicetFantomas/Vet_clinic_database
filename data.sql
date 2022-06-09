6849d2fbcf4bf7e07455176f47b15e723566ae5a
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-06-09 11:08:51 +0200

    Update data.sql file

--- data.sql
+++ data.sql
@@ -8,3 +8,17 @@ INSERT INTO animals (id,name,date_of_birth,escapes_attempts,neutered,weight_kg)
 VALUES(3, 'Pikachu', '2021-07-01', 1, FALSE, 15.04);
 INSERT INTO animals (id,name,date_of_birth,escapes_attempts,neutered,weight_kg)
 VALUES(4, 'Devimon', '2017-05-12', 5, TRUE, 11);
+NSERT INTO animals (id,name,date_of_birth,escapes_attempts,neutered,weight_kg)
+VALUES(5, 'Charmander', '2020-02-08', 0, FALSE, 11);
+INSERT INTO animals (id,name,date_of_birth,escapes_attempts,neutered,weight_kg)
+VALUES(6, 'Plantmon', '2021-11-15', 2, TRUE, -5.7);
+INSERT INTO animals (id,name,date_of_birth,escapes_attempts,neutered,weight_kg)
+VALUES(7, 'Squirtle', '1993-04-02', 3, FALSE, -12.13);
+INSERT INTO animals (id,name,date_of_birth,escapes_attempts,neutered,weight_kg)
+VALUES(8, 'Angemon', '2005-06-12', 1, TRUE, -45);
+INSERT INTO animals (id,name,date_of_birth,escapes_attempts,neutered,weight_kg)
+VALUES(9, 'Boarmon', '2005-06-07', 7, TRUE, 20.4);
+INSERT INTO animals (id,name,date_of_birth,escapes_attempts,neutered,weight_kg)
+VALUES(10, 'Blossom', '1998-09-13', 3, TRUE, 17);
+INSERT INTO animals (id,name,date_of_birth,escapes_attempts,neutered,weight_kg)
+VALUES(11, 'Ditto', '2022-05-14', 4, TRUE, 22);
